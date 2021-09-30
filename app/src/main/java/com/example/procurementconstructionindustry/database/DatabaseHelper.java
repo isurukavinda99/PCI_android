63328d3ce9e69b2830ebd71699d149d864e5d72d
isurukavinda99 <isurukavinda999@gmail.com>
2021-09-30 23:10:14 +0530

    add database java class

---
+++ app/src/main/java/com/example/procurementconstructionindustry/database/DatabaseHelper.java
@@ -0,0 +1,147 @@
+package com.example.procurementconstructionindustry.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "carePlusInfo.db";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = getWritableDatabase();
+        onCreate(db);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        try {
+
+
+//            /*exe crete pms table*/
+//            db.execSQL(DatabaseTable.Patient.CREATE_TABLE_STRING);
+//
+//            db.execSQL(DatabaseTable.Guardian.CREATE_TABLE_STRING);
+//
+//            db.execSQL(DatabaseTable.BeaHeadCard.CREATE_TABLE_STRING);
+//
+//            /*exe create mms tables*/
+//            db.execSQL(DatabaseTable.MealPlan.CREATE_TABLE_STRING);
+//
+//            db.execSQL(DatabaseTable.Portion.CREATE_TABLE_STRING);
+//
+//            db.execSQL(DatabaseTable.PatentHasMealPlan.CREATE_TABLE_STRING);
+//
+//            /*exe create wms tables*/
+//            db.execSQL(DatabaseTable.WorkoutPlan.CREATE_TABLE_STRING);
+//
+//            db.execSQL(DatabaseTable.Exercise.CREATE_TABLE_STRING);
+//
+//            db.execSQL(DatabaseTable.WorkOutReport.CREATE_TABLE_STRING);
+//
+//
+//            /*exe create prms tables*/
+//            db.execSQL(DatabaseTable.Prescription.CREATE_TABLE_STRING);
+//
+//            db.execSQL(DatabaseTable.Drug.CREATE_TABLE_STRING);
+
+        }catch(Exception e){
+            System.out.println("############ database error");
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean save(String TABLE_NAME , ContentValues contentValues){
+        SQLiteDatabase db = getWritableDatabase();
+
+        long result = db.insert(TABLE_NAME , null , contentValues);
+
+        if (result == -1){
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+
+    public Cursor view(String TABLE_NAME , String [] columns , String where , String whereArgs [] , String sortingOrder){
+
+        /*
+            * table name must be existing table name or names
+            * columns is array ["col_name1" , "col_name2"...] if you want to select all then simply ["*"]
+            * where close ex - "where id = 1" convert this to "id ?" !important dont put 'where' and values inside where closes
+            * put null as the parameter when there is no any selection
+            * whereArgs this contains values that map to '?' in whew close  ! please putt null when there is no selection args
+            * sorting order look MAD Lab sheet this same as its sorting order
+            * this method return cursor object
+         */
+
+        SQLiteDatabase db = getWritableDatabase();
+        Cursor result = db.query(
+                TABLE_NAME,
+                columns,
+                where,
+                whereArgs,
+                null,
+                null,
+                sortingOrder
+        );
+
+        return result;
+    }
+
+    public int update(String TABLE_NAME , ContentValues values , String where , String whereArgs[]){
+
+        /*
+            * Table name
+            * values -> this is object of ContentValue , put column name and new value as key and value
+            * where -> "where username like 10" this should be "username like ?" , dont put 'where' and values inside hear
+            * whereArgs -> whereArgs this contains values that map to '?' in whew close
+            * return effected row count
+         */
+
+        SQLiteDatabase db = getWritableDatabase();
+        try{
+            int effected_rows = db.update(
+                    TABLE_NAME ,
+                    values,
+                    where,
+                    whereArgs
+            );
+
+        }catch (Exception e){
+
+        }
+
+
+        return 1;
+    }
+
+    public void delete(String TABLE_NAME , String where , String whereArgs[]){
+
+        /*
+            *where -> "where username like 10" this should be "username like ?" , dont put 'where' and values inside hear
+            *whereArgs -> whereArgs this contains values that map to '?' in whew close
+            *return effected row count
+         */
+
+        SQLiteDatabase db = getWritableDatabase();
+        int effected_rows = db.delete(TABLE_NAME , where , whereArgs);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/procurementconstructionindustry/database/DatabaseTable.java
@@ -0,0 +1,331 @@
+package com.example.procurementconstructionindustry.database;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class DatabaseTable {
+
+    public class Users{
+        /*restrict creating objects*/
+        private Users(){}
+    }
+
+    public class PurchaseOrder{
+        /*restrict creating objects*/
+        private PurchaseOrder(){}
+    }
+
+    public class Items{
+        /*restrict creating objects*/
+        private Items(){}
+    }
+
+//    /*Patient Table*/
+//    public class Patient{
+//
+//        /*restrict creating objects*/
+//        private Patient(){}
+//
+//        /*Table name*/
+//        public static final String TABLE_NAME = "patient";
+//
+//        /*Table properties*/
+//        public static final String PATIENT_ID = "pid";
+//        public static final String PATIENT_FIRST_NAME = "first_name";
+//        public static final String PATIENT_LAST_NAME = "last_name";
+//        public static final String PATIENT_DOB = "dob"; /*Format (dd/mm/yyyy)*/
+//        public static final String PATIENT_REASON = "reason";
+//        public static final String PATIENT_DATE_ADMITTED = "date_admitted";
+//        public static final String PATIENT_ADDITIONAL_INFO = "additional_info";
+//
+//        /*Create String*/
+//        public static final String CREATE_TABLE_STRING = "create table if not exists "+TABLE_NAME +
+//                " ("+PATIENT_ID+" integer not null primary key autoincrement,"
+//                + PATIENT_FIRST_NAME + " varchar(60) not null ,"
+//                + PATIENT_LAST_NAME + " varchar(60) not null , "
+//                + PATIENT_DOB + " date not null ,"
+//                + PATIENT_REASON + " text not null , "
+//                + PATIENT_DATE_ADMITTED + " date not null , "
+//                + PATIENT_ADDITIONAL_INFO + " text )";
+//
+//
+//    }
+//
+//    /*Guardian Table*/
+//    public class Guardian{
+//
+//        /*restrict creating objects*/
+//        private Guardian(){}
+//
+//        /*Table name*/
+//        public static final String TABLE_NAME = "guardian";
+//
+//        /*Table properties*/
+//        public static final String GUARDIAN_ID = "gid";
+//        public static final String GUARDIAN_NAME = "name";
+//        public static final String GUARDIAN_ADDRESS = "address";
+//        public static final String GUARDIAN_CONTACT_NUMBER = "contact_number";
+//        public static final String PATIENT_ID_FK = "pid";
+//
+//        /*Create String*/
+//        public static final String CREATE_TABLE_STRING = "create table if not exists " +TABLE_NAME +
+//                "( "+GUARDIAN_ID+" integer not null primary key  autoincrement, " +
+//                " "+GUARDIAN_NAME+" varchar(60) not null , " +
+//                " "+GUARDIAN_ADDRESS+" text not null , " +
+//                " "+GUARDIAN_CONTACT_NUMBER+" varchar(10) not null , " +
+//                " "+PATIENT_ID_FK+" integer," +
+//                " foreign key("+PATIENT_ID_FK+") references "+Patient.TABLE_NAME+"("+Patient.PATIENT_ID+") on delete cascade on update cascade) ";
+//
+//    }
+//
+//
+//    /*Bed Head Table*/
+//    public class BeaHeadCard{
+//
+//        /*restrict creating objects*/
+//        private BeaHeadCard(){}
+//
+//        /*Table name*/
+//        public static final String TABLE_NAME = "bed_head_card";
+//
+//        /*Table properties*/
+//        public static final String CARD_ID = "id";
+//        public static final String CARD_DATE = "card_date";
+//        public static final String CARD_BLOOD_PRESSURE = "blood_pressure";
+//        public static final String CARD_SUGAR_LEVEL = "sugar_level";
+//        public static final String CARD_DIAGNOSIS = "diagnosis";
+//        public static final String PATIENT_ID_FK = "pid";
+//
+//        /*Create String*/
+//        public static final String CREATE_TABLE_STRING = "create table if not exists "+TABLE_NAME+"" +
+//                "("+CARD_ID+" integer not null primary key autoincrement , " +
+//                " "+CARD_DATE+" date not null , " +
+//                " "+CARD_BLOOD_PRESSURE+" real not null , " +
+//                " "+CARD_SUGAR_LEVEL+" real not null , " +
+//                " "+CARD_DIAGNOSIS+" text , " +
+//                " "+PATIENT_ID_FK+" integer not null , " +
+//                " foreign key("+PATIENT_ID_FK+") references "+Patient.TABLE_NAME+"("+Patient.PATIENT_ID+") on delete cascade on update cascade )";
+//
+//
+//    }
+//
+//
+//    /*meal plan Table*/
+//    public class MealPlan{
+//
+//        /*restrict creating objects*/
+//        private MealPlan(){}
+//
+//        /*Table name*/
+//        public static final String TABLE_NAME = "meal_plan";
+//
+//        /*Table properties*/
+//        public static final String PLAN_ID = "id";
+//        public static final String PLAN_NAME = "plan_name";
+//        public static final String PLAN_TYPE = "plan_type";
+//        public static final String PLAN_DAY = "day";
+//
+//        /*Create String*/
+//        public static final String CREATE_TABLE_STRING = "create table if not exists "+TABLE_NAME+" (" +
+//                " "+PLAN_ID+" integer not null primary key autoincrement , " +
+//                " "+PLAN_NAME+" varchar(60) not null , " +
+//                " "+PLAN_TYPE+" varchar(60) not null , " +
+//                " "+PLAN_DAY+" varchar(20) not null)";
+//
+//
+//    }
+//
+//    /*portion Table*/
+//    public class Portion{
+//
+//        /*restrict creating objects*/
+//        private Portion(){}
+//
+//        /*Table name*/
+//        public static final String TABLE_NAME = "portion";
+//
+//        /*Table properties*/
+//        public static final String PORTION_ID = "id";
+//        public static final String PORTION_NAME = "portion_name";
+//        public static final String PORTION_SIZE = "size";
+//        public static final String PLAN_ID_FK = "plan_id";
+//
+//        /*Create String*/
+//        public static final String CREATE_TABLE_STRING= "create table if not exists "+TABLE_NAME+"(" +
+//                " "+PORTION_ID+" integer not null primary key autoincrement , " +
+//                " "+PORTION_NAME+" varchar(60) not null , " +
+//                " "+PORTION_SIZE+" varchar(60) not null , " +
+//                " "+PLAN_ID_FK+" integer not null ," +
+//                " foreign key("+PLAN_ID_FK+") references "+MealPlan.TABLE_NAME+"("+MealPlan.PLAN_ID+") on delete cascade on update cascade)";
+//
+//
+//    }
+//
+//    /*patent many to may table Table*/
+//    public class PatentHasMealPlan{
+//
+//        /*restrict creating objects*/
+//        private PatentHasMealPlan(){}
+//
+//        /*Table name*/
+//        public static final String TABLE_NAME = "patent_has_meal_plan";
+//
+//        /*Table properties*/
+//        public static final String PATENT_ID = "pid";
+//        public static final String MEAL_PLAN_ID = "plan_id";
+//
+//        /*Create String*/
+//        public static final String CREATE_TABLE_STRING = "create table if not exists "+TABLE_NAME+" (" +
+//                " "+PATENT_ID+" integer not null, " +
+//                " "+MEAL_PLAN_ID+" integer not null , " +
+//                " foreign key("+PATENT_ID+") references "+Patient.TABLE_NAME+"("+Patient.PATIENT_ID+") on delete cascade on update cascade, " +
+//                " foreign key("+MEAL_PLAN_ID+") references "+MealPlan.TABLE_NAME+"("+MealPlan.PLAN_ID+") on delete cascade on update cascade , " +
+//                " primary key("+PATENT_ID+" , "+MEAL_PLAN_ID+"))";
+//
+//
+//    }
+//
+//
+//    /*Workout Plan Table*/
+//    public class WorkoutPlan{
+//
+//        /*restrict creating objects*/
+//        private WorkoutPlan(){}
+//
+//        /*Table name*/
+//        public static final String TABLE_NAME = "workout_plan";
+//
+//        /*Table properties*/
+//        public static final String PLAN_ID = "id";
+//        public static final String PLAN_NAME = "plan_name";
+//        public static final String PLAN_INSTRUCTOR = "instructor";
+//        public static final String PATENT_ID = "pid";
+//
+//        /*Create String*/
+//        public static final String CREATE_TABLE_STRING = "create table if not exists "+TABLE_NAME+"(" +
+//                " "+PLAN_ID+" integer not null primary key autoincrement , " +
+//                " "+PLAN_NAME+" varchar(60) not null , " +
+//                " "+PLAN_INSTRUCTOR+" varchar(60) not null , " +
+//                " "+PATENT_ID+" integer , " +
+//                " foreign key("+PATENT_ID+") references "+Patient.TABLE_NAME+"("+Patient.PATIENT_ID+") on delete set null on update cascade )";
+//
+//    }
+//
+//
+//    /*Exercise Table*/
+//    public class Exercise{
+//
+//        /*restrict creating objects*/
+//        private Exercise(){}
+//
+//        /*Table name*/
+//        public static final String TABLE_NAME = "exercise";
+//
+//        /*Table properties*/
+//        public static final String EXE_ID = "id";
+//        public static final String EXE_NAME = "exercise_name";
+//        public static final String EXE_AREA = "area";
+//        public static final String EXE_STEPS = "steps";
+//        public static final String PLAN_ID = "plan_id";
+//
+//        /*Create String*/
+//        public static final String CREATE_TABLE_STRING = "create table if not exists "+TABLE_NAME+"(" +
+//                " "+EXE_ID+" integer not null primary key autoincrement ," +
+//                " "+EXE_NAME+" varchar(60) not null , " +
+//                " "+EXE_AREA+" varchar(60) not null , " +
+//                " "+EXE_STEPS+" varchar(60) not null ," +
+//                " "+PLAN_ID+" integer not null , " +
+//                " foreign key("+PLAN_ID+") references "+WorkoutPlan.TABLE_NAME+"("+WorkoutPlan.PLAN_ID+") on delete cascade on update cascade ) ";
+//
+//    }
+//
+//
+//    /*Workout Table*/
+//    public class WorkOutReport{
+//
+//        /*restrict creating objects*/
+//        private WorkOutReport(){}
+//
+//        /*Table name*/
+//        public static final String TABLE_NAME = "workout_report";
+//
+//        /*Table properties*/
+//        public static final String REPORT_ID = "id";
+//        public static final String REPORT_DATE = "date";
+//        public static final String REPORT_MONTH = "month";
+//        public static final String REPORT_WEIGHT = "weight";
+//        public static final String PATIENT_ID = "pid";
+//
+//        /*Create String*/
+//        public static final String CREATE_TABLE_STRING = "create table if not exists "+TABLE_NAME+"(" +
+//                " "+REPORT_ID+" integer not null primary key autoincrement , " +
+//                " "+REPORT_DATE+" date not null , " +
+//                " "+REPORT_MONTH+" varchar(10) not null , " +
+//                " "+REPORT_WEIGHT+" real not null ," +
+//                " "+PATIENT_ID+" integer, " +
+//                " foreign key("+PATIENT_ID+") references "+Patient.TABLE_NAME+"("+Patient.PATIENT_ID+") on delete set null on update cascade) ";
+//
+//    }
+//
+//
+//    /*Prescription Table*/
+//    public class Prescription{
+//
+//        /*restrict creating objects*/
+//        private Prescription(){}
+//
+//        /*Table name*/
+//        public static final String TABLE_NAME = "prescription";
+//
+//        /*Table properties*/
+//        public static final String PRESC_ID = "id";
+//        public static final String PRESC_DOCTOR_NAME = "doctor_name";
+//        public static final String PRESC_DOC_MOH_NUMBER = "moh_number";
+//        public static final String PRESC_DESCRIPTION = "description";
+//        public static final String PRESC_TYPE = "type";
+//        public static final String PRESC_ALLOCATED_DATE = "allocated_date";
+//        public static final String PRESC_IS_CURRENT = "is_current";
+//        public static final String PATIENT_ID = "pid";
+//
+//
+//        /*Create String*/
+//        public static final String CREATE_TABLE_STRING = "create table if not exists "+TABLE_NAME+"(" +
+//                " "+PRESC_ID+" integer not null primary key autoincrement , " +
+//                " "+PRESC_DOCTOR_NAME+" varchar(60) not null , " +
+//                " "+PRESC_DOC_MOH_NUMBER+" varchar(10) not null , " +
+//                " "+PRESC_DESCRIPTION+" text not null , " +
+//                " "+PRESC_TYPE+" varchar(10) not null , " +
+//                " "+PRESC_ALLOCATED_DATE+" date not null , " +
+//                " "+PRESC_IS_CURRENT+" boolean not null default false , " +
+//                " "+PATIENT_ID+" integer , " +
+//                " foreign key("+PATIENT_ID+") references "+Patient.TABLE_NAME+"("+Patient.PATIENT_ID+") on delete set null on update cascade )";
+//
+//    }
+//
+//
+//    /*Drug Table*/
+//    public class Drug{
+//
+//        /*restrict creating objects*/
+//        private Drug(){}
+//
+//        /*Table name*/
+//        public static final String TABLE_NAME = "drug";
+//
+//        /*Table properties*/
+//        public static final String DRUG_ID = "drug_id";
+//        public static final String DRUG_NAME = "drug_name";
+//        public static final String DRUG_BEFORE_MEAL = "before_meal";
+//        public static final String PRESCRIPTION_ID = "prescription_id";
+//
+//        /*Create String*/
+//        public static final String CREATE_TABLE_STRING = "create table if not exists "+TABLE_NAME+"(" +
+//                " "+DRUG_ID+" integer not null primary key autoincrement , " +
+//                " "+DRUG_NAME+" varchar(60) not null , " +
+//                " "+DRUG_BEFORE_MEAL+" boolean not null default false , " +
+//                " "+PRESCRIPTION_ID+" integer not null , " +
+//                " foreign key("+PRESCRIPTION_ID+") references "+Prescription.TABLE_NAME+"("+Prescription.PRESC_ID+") on delete cascade on update cascade )";
+//
+//    }
+
+
+}
